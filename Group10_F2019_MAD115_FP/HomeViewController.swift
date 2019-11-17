9f8a55ff0702e2067cc243d2a2101e51d77c4791
Bushair <bushairkm007@gmail.com>
2019-11-17 01:32:51 -0500

    Case 2 : Instantiate ViewEmployeeController

--- Group10_F2019_MAD115_FP/HomeViewController.swift
+++ Group10_F2019_MAD115_FP/HomeViewController.swift
@@ -1,98 +1,21 @@
 //
-//  HOMETableViewController.swift
-//  Group3_F2019_MAD115_FP
+//  HomeViewController.swift
+//  Group10_F2019_MAD115_FP
 //
-//  Created by MacStudent on 2019-11-14.
-//  Copyright © 2019 MacStudent. All rights reserved.
+//  Created by Akhil Joseph on 2019-11-17.
+//  Copyright © 2019 Bushair. All rights reserved.
 //
 
 import UIKit
 
-class HomeTableViewController: UITableViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return  section == 0 ? 6:6
+        // Do any additional setup after loading the view.
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-                switch indexPath.row {
-                       case 0:
-
-                           let stbHome=UIStoryboard(name: "Main", bundle: nil)
-                           let NHomeVC = stbHome.instantiateViewController(withIdentifier: "NHomeVC") as! ViewController
-                                  navigationController?.pushViewController(NHomeVC, animated: true)
-                       case 1:
-                          
-                           let stbHome=UIStoryboard(name: "Main", bundle: nil)
-                           let empVC = stbHome.instantiateViewController(withIdentifier: "empVC") as! ViewEmployeeViewController
-                                  navigationController?.pushViewController(empVC, animated: true)
-                       case 2:
-                    navigationController?.popViewController(animated: true)
-                       default:
-                       print("invalid click")
-                       
-         
-    }
-    }
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
@@ -105,4 +28,3 @@ class HomeTableViewController: UITableViewController {
     */
 
 }
-
