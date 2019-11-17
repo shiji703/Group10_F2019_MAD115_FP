9aad939c3a1e6c2f221d16242663844a1a2e0aff
Bushair <bushairkm007@gmail.com>
2019-11-17 02:52:00 -0500

    Label Employee Type

--- Group10_F2019_MAD115_FP/ViewEmployeeViewController.swift
+++ Group10_F2019_MAD115_FP/ViewEmployeeViewController.swift
@@ -17,8 +17,7 @@ class ViewEmployeeViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
+    
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
